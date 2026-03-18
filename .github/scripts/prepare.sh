f20b13bc7ac67d7ff3ec919bcb21c7115a44cbb7
Alex Archambault <alexandre.archambault@gmail.com>
2026-03-18 17:30:35 +0100

    fixup

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -13,7 +13,7 @@ elif [ "$(uname)" == "Darwin" ]; then
   brew install ninja libslirp
   pip3 install --break-system-packages distlib
 elif [[ `uname | grep -E 'CYG*|MSYS*|MING*|UCRT*|ClANG*|GIT*'` ]]; then
-  pip install meson==1.5.0
+  pip install meson==1.5.0 pycotap==1.3.1
   curl -fLO https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-libslirp-4.8.0-2-any.pkg.tar.zst
   pacman --noconfirm -U mingw-w64-x86_64-libslirp-4.8.0-2-any.pkg.tar.zst
   echo cd "$MINGW_PREFIX/include"
